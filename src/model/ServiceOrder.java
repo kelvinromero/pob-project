422c052c2ee58879de811d90d3b9794849c452e2
Peter Costa <petersvcosta@gmail.com>
2022-12-17 21:32:39 -0300

    Update ServiceOrder.java

--- src/model/ServiceOrder.java
+++ src/model/ServiceOrder.java
@@ -82,14 +82,6 @@ public class ServiceOrder {
     public void setId(int id) {
         this.id = id;
     }
-    
-    public String getDate() {
-        return date;
-    }
-
-    public void setDate(String date) {
-        this.date = date;
-    }
 
     @Override
     public String toString() {
